6db3e51da9d18276f9c4b99ae907ab4ff1a6f827
Al Shaharia <ifty682002@gmail.com>
2022-11-08 14:40:57 +0600

    get one service are working

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vtk80fb.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -29,7 +29,14 @@ async function run() {
       const services = await servicesCollection.find(query).toArray();
       res.send(services);
     });
-    //
+
+    //   get one service
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await servicesCollection.findOne(query);
+      res.send(service);
+    });
   } finally {
   }
 }
