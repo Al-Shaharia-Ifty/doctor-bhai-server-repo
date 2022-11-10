a59d47932d4fd8d44c315da3326b03cc027159ff
Al Shaharia <ifty682002@gmail.com>
2022-11-10 18:35:49 +0600

    delete user review in done

--- index.js
+++ index.js
@@ -91,6 +91,15 @@ async function run() {
       res.send(result);
     });
 
+    // delete user review
+    app.delete("/my-review/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewCollection.deleteOne(query);
+      console.log(result);
+      res.send(result);
+    });
+
     //   login user
     app.put("/user/:email", async (req, res) => {
       try {
